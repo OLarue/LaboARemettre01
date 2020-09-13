4e15d7a1ba26e841a0f2101a5eadd7108ff8bc25
OlivierLarue <1765918@cstj.qc.ca>
2020-09-13 17:34:32 -0400

    Ajoutez des fichiers projet.

---
+++ Exercice1/PairImpair.cpp
@@ -0,0 +1,44 @@
+// But: Écrire un programme qui lit un entier et qui détermine et affiche s’il s’agit d’un entier pair d’un entier impair
+// Auteur: Olivier Larue
+// Date: 14 septembre 2020
+
+
+#include <iostream>
+
+void main()
+{ 
+	setlocale(LC_ALL, "");    // Pour convertir les accents dans la console
+
+	// Variable nb1 qui va être déterminé par l'utilisateur
+
+	int nb1;
+
+	//Programme demande un entier à l'utilisateur
+	//On invite l'utilisateur a entrer un entier
+	 
+	std::cout << " veuillez entrer un nombre entier: ";
+	// le programme lit le nombre sur le clavier 
+	std::cin >> nb1; 
+	// Si l'utilisateur met un nombre pair il restera 0 lors du modulo (%) donc le nombre est pair
+	if (nb1 % 2 == 0)
+	{
+		std::cout << nb1 << " Est pair ";
+	}
+	// Si l'utilisateur met un nombre impair il restera 1 lors du modulo (%) donc le nombre est impair
+	else
+	{
+		std::cout << nb1 << " Est impair ";
+	}
+}
+
+
+/* Plan test:
+nb1      résultat
+
+23        Impair 
+0         pair
+-1        impair 
+-123434   pair
+150897999123   impair
+14        pair
+*/
---
+++ projet_Exercice2/Vertical.cpp
@@ -0,0 +1,42 @@
+// but:  Un programme qui prend comme entrée un nombre de cinq chiffres. Le programme vérifie qu'il y a bien
+// 5 chiffres dans le nombre.Puis il affiche le nombre verticalement, le premier chiffre sur la première ligne, le
+// deuxième chiffre sur la deuxième ligne, ainsi de suite
+// Auteur: Olivier Larue 
+// Date: 14 septembre 2020
+
+
+
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, ""); //pour convertir les accents dans la console
+
+	int nombre;
+	// On demande à l'utilisateur d'entrer un nombre à 5 chiffres.
+	std::cout << "veuillez entrer un nombre à 5 chiffres. " << ".";
+	// le programme lit le nombre entrer par l'utilisateur.
+	std::cin >> nombre;
+
+	if (nombre >= 10000)
+	{
+		std::cout << nombre / 10000;
+	}
+
+	else
+	{
+		std::cout << "Ce nombre ne contient pas 5 chiffres.";
+	}
+	if (nombre >= 10000)
+	{
+		std::cout << nombre / 1100;
+	}
+	else
+	{
+		std::cout << " ce nombre ne contient pas 5 chiffres.";
+
+	}
+}
+
+
+// je n'ai pas réussir à trouver les autres équations pour aligner les chiffres verticalement :( mais au moins mon premier programme fonctionne bien! =D  
